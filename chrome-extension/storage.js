b2ef48f2d9da30722327877697a6d1e49c2d20d2
wk <wk.cvs.github@sydorenko.org.ua>
2012-08-25 21:46:30 +0300

    localStorage wrapper added

--- chrome-extension/storage.js
+++ chrome-extension/storage.js
@@ -4,12 +4,55 @@
 
 //This file depends on JQuery $.totalStorage
 
-// wK
+// (c) wk
+//
+// USAGE:
+// assuming there is following entry in localStorage:
+// test     |   {'ololo': 100500, 'tmp': {'sdf': 'ololo', 'k': 'v'}}
+// $storage.get('test.tmp.k')           // will return "v"
+// $storage.set('test.tmp.foo': 'bar')  // will change previous to 
+// test     |   {'ololo': 100500, 'tmp': {'sdf': 'ololo', 'k': 'v', 'foo': 'bar'}}
 
-$(function(){
-    var $storage = {
-            '': function(){
-                
+var $storage = {
+        'get': function(path){
+            var spath = path.split('.');
+            
+            if (spath.length < 1)
+                return null;
+            
+            var res = $.totalStorage(spath.shift());
+
+            if (typeof(res) === 'undefined')
+                return null;
+            
+            if (spath.length < 1)
+                return res;
+            
+            while (typeof(p = spath.shift()) !== 'undefined')
+            {
+                res = res[p];
+                if (typeof(res) === 'undefined')
+                    return null;
             }
-    }
-});
+            
+            return res;
+        },
+        'set': function(path, value){
+            var spath = path.split('.');
+            var key = spath.shift();
+            var val = $.totalStorage(key);
+            var v = value;
+            
+            while (typeof(p = spath.pop()) !== 'undefined')
+            {
+                var tmp = v;
+                v = {};
+                v[p] = tmp;
+            }
+            
+            if (typeof(val) !== 'undefined')
+                $.extend(true, v, val);
+            
+            $.totalStorage(key, v);
+        }
+};
