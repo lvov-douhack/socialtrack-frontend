92f375217a37e0be99e5cd37f601c984d73a8347
unknown <wK@netKnjaZ.(none)>
2012-08-25 19:37:41 +0300

    background.html and storage.js added / TODO: write Local Storage wrapper and refactor bachground.html

---
+++ chrome-extension/storage.js
@@ -0,0 +1,15 @@
+﻿//Copyright (c) 2012  #douhack
+//authors: wk, G3D, vessi, zasadnyy
+//vim: set sw=2 sts=2 ts=8 et syntax=javascript:
+
+//This file depends on JQuery $.totalStorage
+
+// wK
+
+$(function(){
+    var $storage = {
+            '': function(){
+                
+            }
+    }
+});
