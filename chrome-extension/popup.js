94a583db7956b166a3ba3421cb6ffc75d7316a75
unknown <wK@netKnjaZ.(none)>
2012-08-25 17:54:55 +0300

    Basic structure created

---
+++ chrome-extension/popup.js
@@ -0,0 +1,34 @@
+﻿//Copyright (c) 2012  #douhack
+//authors: wk, G3D, vessi, zasadnyy
+//vim: set sw=2 sts=2 ts=8 et syntax=javascript:
+
+//This file depends on JQuery
+
+// wK
+
+function OpenLink(url)
+{
+    if (typeof(url) !== 'undefined')
+        chrome.tabs.create({url: url});
+    else    
+        throw new Exception('Invalid URL!');
+}
+
+function ClosePopup()
+{
+  window.close();
+}
+
+function OpenURLFromElem()
+{
+  if (event && event.target)
+  {
+    var url = event.currentTarget.getAttribute("url");
+    if (url)
+      OpenLink(url);
+  }
+}
+
+$(function(){
+  
+});
