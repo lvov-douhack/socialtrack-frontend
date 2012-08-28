b81e05878876d86de35bd3ef41372f639852bc08
wk <wk.cvs.github@sydorenko.org.ua>
2012-08-28 16:37:30 +0300

    recursive storage fixed

--- chrome-extension/storage.js
+++ chrome-extension/storage.js
@@ -12,16 +12,18 @@
 // $.storage.get('test.tmp.k')           // will return "v"
 // $.storage.set('test.tmp.foo': 'bar')  // will change previous to 
 // test     |   {'ololo': 100500, 'tmp': {'sdf': 'ololo', 'k': 'v', 'foo': 'bar'}}
+// $.storage.inc('ololo', 100500)  // will change previous to 
+// test     |   {'ololo': 201000, 'tmp': {'sdf': 'ololo', 'k': 'v', 'foo': 'bar'}}
 
 $.storage = {
-        'get': function(path){
+        'get': function(path) {
             var spath = path.split('.');
             
             if (spath.length < 1)
                 return null;
             
             var res = $.totalStorage(spath.shift());
-
+            
             if (typeof(res) === 'undefined')
                 return null;
             
@@ -39,10 +41,12 @@ $.storage = {
             
             return res;
         },
-        'set': function(path, value){
+        'set': function(path, value) {
             var spath = path.split('.');
             var key = spath.shift();
             var val = $.totalStorage(key);
+            if (typeof(val) === 'undefined')
+				return;
             var v = value;
             
             while (typeof(p = spath.pop()) !== 'undefined')
@@ -52,15 +56,49 @@ $.storage = {
                 v[p] = tmp;
             }
             
-            if (typeof(val) !== 'undefined')
-                $.extend(true, v, val);
+            //console.log('v = ' + v);
+            //console.log('val = ' + val);
+            $.extend(true, val, v);
+            //console.log('v = ' + v);
+            //console.log('val = ' + val);
             
-            $.totalStorage(key, v);
+            $.totalStorage(key, val); // val?
         },
-        'inc': function(path, inc){
-            if (typeof(inc) !== 'undefined')
-                inc = 1;
-            
-            $.storage.set(path, (typeof($.storage.get(path)) !== 'undefined' ? parseInt($.storage.get(path)) : 0) + parseInt(inc));
+        'inc': function(path, inc) {
+            switch (typeof(inc))
+            {
+				case 'undefined':
+					inc = 1;
+					break;
+				case 'number':
+					break;
+				default:
+					inc = parseInt(inc);
+			}
+			
+			//console.log(inc);
+			//console.log(typeof inc);
+			
+            var prev = $.storage.get(path);
+            switch (typeof(prev))
+            {
+				case 'undefined':
+					prev = 0;
+					break;
+				case 'number':
+					break;
+				default:
+					prev = parseInt(prev);
+			}
+			
+			//console.log(prev);
+			//console.log(typeof prev);
+			
+			var res = prev + inc;
+			
+			//console.log(res);
+			//console.log(typeof res);
+			
+            $.storage.set(path, res);
         }
 };
