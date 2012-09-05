45236b6c5004c5a496ba76db82e0236ed71408dc
wk <wk.cvs.github@sydorenko.org.ua>
2012-09-05 14:58:53 +0300

    1.0.2 version of $.storage plugin added.

--- chrome-extension/storage.js
+++ chrome-extension/storage.js
@@ -1,94 +1,111 @@
-﻿//Copyright (c) 2012  #douhack
-//authors: wk, G3D, vessi, zasadnyy
-//vim: set sw=2 sts=2 ts=8 et syntax=javascript:
+﻿// Copyright (c) 2012 #douhack
+// @author: webknjaz
+// @version: 1.0.2 Alpha
+// @license: GNU/GPL v3
+// vim: set sw=2 sts=2 ts=8 et syntax=javascript:
 
-//This file depends on JQuery $.totalStorage
+// This file depends on JQuery $.totalStorage
 
-// (c) wk
-//
-// USAGE:
-// assuming there is following entry in localStorage:
-// test     |   {'ololo': 100500, 'tmp': {'sdf': 'ololo', 'k': 'v'}}
-// $.storage.get('test.tmp.k')           // will return "v"
-// $.storage.set('test.tmp.foo': 'bar')  // will change previous to 
-// test     |   {'ololo': 100500, 'tmp': {'sdf': 'ololo', 'k': 'v', 'foo': 'bar'}}
-// $.storage.inc('ololo', 100500)  // will change previous to 
-// test     |   {'ololo': 201000, 'tmp': {'sdf': 'ololo', 'k': 'v', 'foo': 'bar'}}
+/***********************************************************************************
+ *                                                                                 *
+ * (c) 2012, wk                                                                    *
+ *                                                                                 *
+ * USAGE:                                                                          *
+ * assuming there is following entry in localStorage:                              *
+ * test     |   {'ololo': 100500, 'tmp': {'sdf': 'ololo', 'k': 'v'}}               *
+ * $.storage.get('test.tmp.k')             // will return "v"                      *
+ * $.storage.set('test.tmp.foo': 'bar')    // will change previous to              *
+ * test     |   {'ololo': 100500, 'tmp': {'sdf': 'ololo', 'k': 'v', 'foo': 'bar'}} *
+ * $.storage.inc('test.ololo', 100500)          // will change previous to         *
+ * test     |   {'ololo': 201000, 'tmp': {'sdf': 'ololo', 'k': 'v', 'foo': 'bar'}} *
+ *                                                                                 *
+ ***********************************************************************************/
 
 $.storage = {
-        'get': function(path) {
-            var spath = path.split('.');
-            
-            if (spath.length < 1)
-                return null;
-            
-            var res = $.totalStorage(spath.shift());
-            
-            if (typeof(res) === 'undefined')
-                return null;
-            
-            if (spath.length < 1)
-                return res;
-            
-            while (typeof(p = spath.shift()) !== 'undefined')
-            {
-                if (typeof(res) === 'undefined' || res === null)
-                    return null;
-                res = res[p];
-                if (typeof(res) === 'undefined')
-                    return null;
-            }
-            
-            return res;
-        },
-        'set': function(path, value) {
-            var spath = path.split('.');
-            var key = spath.shift();
-            var val = $.totalStorage(key);
-            if (typeof(val) === 'undefined')
-				return;
-            var v = value;
-            
-            while (typeof(p = spath.pop()) !== 'undefined')
-            {
-                var tmp = v;
-                v = {};
-                v[p] = tmp;
-            }
-            
-            //console.log('v = ' + v);
-            //console.log('val = ' + val);
-            $.extend(true, val, v);
-            //console.log('v = ' + v);
-            //console.log('val = ' + val);
-            
-            $.totalStorage(key, val); // val?
-        },
-        'inc': function(path, inc) {
-            switch (typeof(inc))
-            {
+		'version': '1.0.2',
+		'author': 'webknjaz',
+		'get': function(path) {
+			var spath = path.split('.');
+			
+			if (spath.length < 1)
+				return null;
+			
+			var res = $.totalStorage(spath.shift());
+			
+			if (typeof(res) === 'undefined')
+				return null;
+			
+			if (spath.length < 1)
+				return res;
+			
+			while (typeof(p = spath.shift()) !== 'undefined')
+			{
+				if (typeof(res) === 'undefined' || res === null)
+					return null;
+				res = res[p];
+				if (typeof(res) === 'undefined')
+					return null;
+			}
+			
+			return res;
+		},
+		'set': function(path, value) {
+			var spath = path.split('.');
+			var key = spath.shift();
+			var val = $.totalStorage(key);
+
+			
+			if (typeof(val) === 'number' && val >= 10000000000000000)
+				throw new Error('Out of range');
+			
+			var v = value;
+			
+			while (typeof(p = spath.pop()) !== 'undefined')
+			{
+				var tmp = v;
+				v = {};
+				v[p] = tmp;
+			}
+			
+			//console.log('v = ' + v);
+			//console.log('val = ' + val);
+			val = $.extend(true, {}, val, v);
+			//console.log('v = ' + v);
+			//console.log('val = ' + val);
+			
+			$.totalStorage(key, val); // val?
+		},
+		'inc': function(path, inc) {
+			switch (typeof(inc))
+			{
 				case 'undefined':
 					inc = 1;
 					break;
 				case 'number':
 					break;
 				default:
-					inc = parseInt(inc);
+					if (inc == null)
+						inc = 0;
+					else
+						inc = parseInt(inc);
 			}
 			
 			//console.log(inc);
 			//console.log(typeof inc);
 			
-            var prev = $.storage.get(path);
-            switch (typeof(prev))
-            {
+			var prev = $.storage.get(path);
+			switch (typeof(prev))
+			{
 				case 'undefined':
 					prev = 0;
 					break;
 				case 'number':
 					break;
 				default:
-					prev = parseInt(prev);
+					if (prev == null)
+						prev = 0;
+					else
+						prev = parseInt(prev);
 			}
 			
 			//console.log(prev);
@@ -99,6 +116,9 @@ $.storage = {
 			//console.log(res);
 			//console.log(typeof res);
 			
-            $.storage.set(path, res);
-        }
+			if (res == prev)
+				throw new Error('Out of range');
+
+			$.storage.set(path, res);
+		}
 };
